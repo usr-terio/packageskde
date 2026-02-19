09dc6350d4bb432d10ece656a735eae82669b809
usr-terio <leoteriod@outlook.com>
2026-02-18 23:11:01 -0500

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S steam gamemode gamescope fwupd fwupd-docs sbctl efitools pacman-contrib unrar unzip ufw gwenview firefox blender bitwarden openh264 terminus-font evolution evince file-roller apparmor
+pacman -S steam gamemode gamescope fwupd fwupd-docs sbctl efitools pacman-contrib unrar unzip ufw gwenview firefox blender bitwarden openh264 terminus-font evolution evince file-roller apparmor dnscrypt-proxy
 
 rm -rf /usr/share/sddm/themes/maya
 /usr/share/applications/assistant.desktop /usr/share/applications/designer.desktop /usr/share/applications/linguist.desktop /usr/share/applications/qdbusviewer.desktop /usr/share/applications/qv4l2.desktop /usr/share/applications/qvidcap.desktop /usr/share/applications/lstopo.desktop /usr/share/applications/org.kde.kwrite.desktop 
