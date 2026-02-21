1c126619eb6aedf42dbbcbe53e32178caca3cf83
usr-terio <leoteriod@outlook.com>
2026-02-20 21:11:09 -0500

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -4,5 +4,4 @@ pacman -Sy steam gamemode gamescope fwupd fwupd-docs sbctl efitools pacman-contr
 
 pacman -Rs htop ark
 
-rm -rf /usr/share/sddm/themes/maya
-/usr/share/applications/assistant.desktop /usr/share/applications/designer.desktop /usr/share/applications/linguist.desktop /usr/share/applications/qdbusviewer.desktop /usr/share/applications/qv4l2.desktop /usr/share/applications/qvidcap.desktop /usr/share/applications/lstopo.desktop /usr/share/applications/org.kde.kwrite.desktop
+rm -rf /usr/share/sddm/themes/maya /usr/share/applications/assistant.desktop /usr/share/applications/designer.desktop /usr/share/applications/linguist.desktop /usr/share/applications/qdbusviewer.desktop /usr/share/applications/qv4l2.desktop /usr/share/applications/qvidcap.desktop /usr/share/applications/lstopo.desktop /usr/share/applications/org.kde.kwrite.desktop
