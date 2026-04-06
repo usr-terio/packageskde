0016b3898b7b575b8306ed637fe9a083acda557d
usr-terio <leoteriod@outlook.com>
2026-04-05 20:59:01 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S steam gamemode gamescope sddm-kcm kded kded5 fuse2 fwupd fwupd-docs sbctl efitools pacman-contrib unrar unzip ufw gwenview firefox blender bitwarden openh264 terminus-font evolution evince btrfs-assistant apparmor dnscrypt-proxy
+pacman -S steam gamemode gamescope kded kded5 fuse2 fwupd fwupd-docs sbctl efitools pacman-contrib unrar unzip ufw gwenview firefox blender bitwarden openh264 terminus-font evolution evince btrfs-assistant apparmor dnscrypt-proxy
 
 pacman -S --needed base-devel
 
