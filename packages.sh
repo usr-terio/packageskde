d8d17a54139c79989318e50cfa90b21fefb63f44
usr-terio <leoteriod@outlook.com>
2026-04-21 21:43:40 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -2,8 +2,6 @@
 
 pacman -S steam gamemode gamescope fuse2 fwupd fwupd-docs sbctl efitools pacman-contrib unrar unzip ufw gwenview firefox blender bitwarden openh264 terminus-font evolution evince btrfs-assistant apparmor dnscrypt-proxy
 
-pacman -S nvidia-open-dkms dkms libva-nvidia-driver
-
 pacman -S --needed base-devel
 
 pacman -Rn htop vim
